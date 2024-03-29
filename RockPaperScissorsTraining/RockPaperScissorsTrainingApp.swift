d6ffb68d31fec93e24c6be31b519221fe960c9e3
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-03-29 19:17:28 +0100

    Initial Commit

---
+++ RockPaperScissorsTraining/RockPaperScissorsTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsTrainingApp.swift
+//  RockPaperScissorsTraining
+//
+//  Created by MaćKo on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
